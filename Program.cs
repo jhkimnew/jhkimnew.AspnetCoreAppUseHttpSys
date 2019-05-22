a60e300f550081fbf84f1eed10ab2219f852f5da
Jeong Hwan Kim <jhkim@microsoft.com>
2019-05-22 12:52:57 -0700

    Imported

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server.HttpSys;
+
+namespace aspnetcoreapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = CreateWebHostBuilder(args);
+            var host = builder.Build();
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls("https://*:44302")
+                .UseStartup<Startup>()
+                .UseHttpSys(o => {
+                     o.Authentication.Schemes = AuthenticationSchemes.NTLM | AuthenticationSchemes.Negotiate;
+                     o.Authentication.AllowAnonymous = false;
+                 })
+                ;
+    }
+}
